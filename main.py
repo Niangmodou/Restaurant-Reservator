e400f4799def7b0cdc6d592dbaff5ee9948b0ba3
Niangmodou <niangmodou100@gmail.com>
2022-01-07 14:11:40 -0600

    Reformatted main.py

--- main.py
+++ main.py
@@ -19,23 +19,37 @@ service = Service("./chromedriver")
 options = webdriver.ChromeOptions()
 DRIVER = webdriver.Chrome(service=service, options=options)
 
+
 def find_reservation(month, day) -> str:
-    month = '0' + str(month)
+    month = "0" + str(month)
     if day < 10:
-        day = '0' + str(day)
-    link = "https://resy.com/cities/ny/carbone?date=2022-{}-{}&seats=2".format(month, day)
+        day = "0" + str(day)
+    link = "https://resy.com/cities/ny/carbone?date=2022-{}-{}&seats=2".format(
+        month, day
+    )
     print("MAKING CALL TO:", link)
     DRIVER.get(link)
 
     time.sleep(2)
 
-    DRIVER.find_element(By.XPATH, "/html/body/div[1]/main/div/div[2]/div/article[1]/section[1]/resy-inline-booking/div/div/div[3]/div/resy-reservation-button-list")
+    DRIVER.find_element(
+        By.XPATH,
+        "/html/body/div[1]/main/div/div[2]/div/article[1]/section[1]/resy-inline-booking/div/div/div[3]/div/resy-reservation-button-list",
+    )
 
     return link
 
+
 def send_email(month, day, link):
     month_map = {1: "January", 2: "February"}
-    message = "CARBONE HAS AN OPENING ON " + month_map[month] + " " + str(day) + ". Make a reservation at: " + link
+    message = (
+        "CARBONE HAS AN OPENING ON "
+        + month_map[month]
+        + " "
+        + str(day)
+        + ". Make a reservation at: "
+        + link
+    )
 
     # Creating the email
     sender = "niangmodou100@gmail.com"
@@ -47,11 +61,12 @@ def send_email(month, day, link):
     msg["To"] = ",".join(receivers)
 
     # Sending the email using Simple Mail Transfer Protocool
-    s = smtplib.SMTP_SSL(host = "smtp.gmail.com", port = 465)
-    s.login(user = EMAIL, password = PASSWORD)
+    s = smtplib.SMTP_SSL(host="smtp.gmail.com", port=465)
+    s.login(user=EMAIL, password=PASSWORD)
     s.sendmail(sender, receivers, msg.as_string())
     s.quit()
     print("SUCCESS!")
+
 
 def main():
     for month in range(1, 3):
@@ -63,7 +78,7 @@ def main():
 
             except Exception as _:
                 print("NOT FOUND:")
-                
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
